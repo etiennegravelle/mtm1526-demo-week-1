703a88ae234bd8e0a22ffc7edbaccc78d7cded8c
etiennegravelle <etienne.gravelle@gmail.com>
2012-01-13 11:37:50 -0500

    1st commit

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+alert('this is an alert!'); //this pops up an alert with an ok button
+
+var name = prompt('what is your name?');
+
+//'console.log()' writes out the content of a variable for debugging
+//identical to Actionscript's 'trace()'
+
+console.log(name);
+
+document.write('Hello, ' + name);
