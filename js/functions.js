e300aa3501ed9cb69475b02b0e7f123bb2d1f8dc
etiennegravelle <etienne.gravelle@gmail.com>
2012-01-13 12:28:45 -0500

    2nd version

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,11 @@
 
 alert('this is an alert!'); //this pops up an alert with an ok button
 
-var name = prompt('what is your name?');
+var name;
+
+while(!name) {
+	name = prompt('what is your name?');
+}
 
 //'console.log()' writes out the content of a variable for debugging
 //identical to Actionscript's 'trace()'
@@ -10,3 +14,30 @@ var name = prompt('what is your name?');
 console.log(name);
 
 document.write('Hello, ' + name);
+
+for (var i = 0; i < 5; i++) {
+	document.write('<br>' + i);
+	document.write('<h1>' + i + '</h1>');
+}
+
+function isNameCool () {
+	var name = prompt('What is your name?')
+		
+	if (name == 'Etienne') {
+		document.write('that is an awesome name!');
+	}else{
+		document.write("I don't like your name!");
+	}
+}
+
+isNameCool();
+
+var writeNameMultipleTimes = function () {
+	for(var i = 0; i < 5; i++ ) {
+		document.write('<strong>Etienne</strong><br>');
+	}
+	
+};
+
+writeNameMultipleTimes();
+			
